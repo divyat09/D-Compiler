90a7d844f6a46578a18b00a75234b8c0095333ab
poojay1 <poojay25101996@gmail.com>
2018-02-04 19:45:52 +0530

    Create irds.py

---
+++ asgn2/src/irds.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+class irds:
+  def __init__(self,data):
+    self.lineno = 0
+    self.op = None
+    self.dst = None
+    self.src1 = None
+    self.src2 = None
+    
+    
+    
+  def represent(self,data):
+  	self.lineno = data[0]
+    self.op = data[1]
+    if (data[1] == "ifgoto"):
+      self.src1 =  data[2]
+      self.dst = data[3]
+      bb.append(int(self.lineno+1))
+      bb.append(int(self.dst))
+    elif (data[1] = "jmp"):
+      self.dst = data[2]
+      bb.append(int(self.lineno+1))
+      bb.append(int(self.dst))
+    elif (data[1] == "call"):
+      self.dst = data[2]
+      # what about basic block???
+    elif (data[1] == "return"):  
+      self.src1 = data[2]
+    elif (data[1] == "="):
+      self.dst = data[2]
+      self.src1 = data[3]
+    elif (data[1] = "!"):
+      self.dst = data[2]
+      self.src1 = data[3]
+    elif (data[1] == "print"):
+      ??
+    elif (data[1] == "input"):
+      ??
+    else:
+      self.dst = data[2]
+      self.src1 = data[3]
+      self.src2 = data[4]
