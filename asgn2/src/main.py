aa67c0c31df4e34216710f360b42fb1c0d2d5ff9
divyat09 <divyatcricket@gmail.com>
2018-02-07 21:45:17 +0530

    Updated main.py

--- asgn2/src/main.py
+++ asgn2/src/main.py
@@ -1,4 +1,43 @@
-# Change the filename
+#!/usr/bin/env python2
+class IRDS:
+  def __init__(self):
+    self.lineno = 0
+    self.op = None
+    self.dst = None
+    self.src1 = None
+    self.src2 = None
+        
+  def represent(self,data):
+  	self.lineno = data[0]
+    self.op = data[1]
+    if (data[1] == "ifgoto"):
+      self.src1 =  data[2]
+      self.dst = data[3]
+      bb.append(int(self.lineno+1))
+      bb.append(int(self.dst))
+    elif (data[1] = "jmp"):
+      self.dst = data[2]
+      bb.append(int(self.lineno+1))
+      bb.append(int(self.dst))
+    elif (data[1] == "call"):
+      self.dst = data[2]
+      # what about basic block???
+    elif (data[1] == "return"):  
+      self.src1 = data[2]
+    elif (data[1] == "="):
+      self.dst = data[2]
+      self.src1 = data[3]
+    elif (data[1] = "!"):
+      self.dst = data[2]
+      self.src1 = data[3]
+    elif (data[1] == "print"):
+      self.src1 = data[2]
+    elif (data[1] == "input"):
+      self.src1 = data[2]
+    else:
+      self.dst = data[2]
+      self.src1 = data[3]
+      self.src2 = data[4]
 
 f=open(filename, 'r')
 for line in f.readlines():
