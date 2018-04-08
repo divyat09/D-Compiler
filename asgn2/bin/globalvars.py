9e40f82df1f2f8f137ee05822bd968fae3538bca
divyat09 <divyatmahajan@gmail.com>
2018-04-08 15:12:15 +0530

    changes for pull

---
+++ asgn2/bin/globalvars.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+from symbol_table import *
+
+def isint(value):
+  if (value[0]=='-'):
+    value = value[1:]
+    return value.isdigit()
+  else:
+    return value.isdigit()
+
+bb = [] #list for basic block
+statements = [] #list of IR objects for each statement
+NextUseTable= []
+Table = SymbolTable()
+functions = [] #stores function names
+labels = {} #stores names of labels if they are line_no as labelline_no
+RegisterStatus={ '%ebx':-1, '%ecx':-1, '%esi':-1, '%edi':-1, '%eax':-1, '%edx':-1 }   
+RegisterData= {'%ebx':None, '%ecx':None, '%esi':None, '%edi':None, '%eax':None, '%edx':None }   
+RegisterAssigned= {  }
+
+op2wrd= { '+': 'addl', '-': 'subl', '*': 'imul', '/': 'divl', '%': 'modl', '<<': 'shll', '>>': 'shrl', '&': 'andl', '|': 'orl', '^':'xorl',
+		'ifgoto_gt':'jg', 'ifgoto_geq':'jge' , 'ifgoto_lt':'jl', 'ifgoto_leq':'jle', 'ifgoto_eq':'je', 'ifgoto_neq':'jne' }
+
+ArraySize= 10
+
+AssemFile='AssemblyCode.S'
--- asgn3/Final/asgn3/test/func.d
+++ asgn3/Final/asgn3/test/func.d
@@ -4,9 +4,6 @@ int foo(int x,float y, char z)
 }
 
 int main(){
-    int a = 2;
-    float b = 13.776;
-    char c = 'c';
     res = foo(a,b,c);
     return 0;
 }
