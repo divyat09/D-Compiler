2ef3ab1179d2665ca6550b1ddfb41e51b6afc0b0
prithvi000 <prithvirajchavan33@gmail.com>
2018-04-17 16:44:40 +0530

    Symbol Table updated to support scoping

--- Final Project/src/symbol_table.py
+++ Final Project/src/symbol_table.py
@@ -6,6 +6,8 @@ class SymbolTable:
 		self.table = {}
 		self.temp_no=0	
 		self.label_no=0
+		self.currentscope = 'main'
+		self.scope_no = 0
 		#Add variable to table 
 
 	def get_temp(self):
@@ -19,9 +21,40 @@ class SymbolTable:
 	def addvar(self,varname,datatype,type,size):
 		if '[' in varname:
 			varname= varname.split('[')[0]
-			self.table[varname]= {'name': varname,'type':'Array','scope':None}
+			self.table[self.currentscope]['identifiers'][varname]= {'name': varname,'type':'Array','scope':self.currentscope}
 		else:
-			self.table[varname]={'name':varname,'type':type,'datatype':datatype,'scope':None,'size':size}
+			self.table[self.currentscope]['identifiers'][varname]={'name':varname,'type':type,'datatype':datatype,'scope':self.currentscope,'size':size}
 
 	def addfunc(self,funcname,_type,datatype):
-		self.table[funcname]={'name':funcname,'type':_type,'datatype':datatype,'scope':None}
+		parentscope = self.currentscope
+		if funcname=='main':
+			self.table[funcname]={'name':funcname,'type':_type,'datatype':datatype,'parentscope':None,'identifiers':{}}
+		else:	
+			self.table[funcname]={'name':funcname,'type':_type,'datatype':datatype,'parentscope':parentscope,'identifiers':{}}
+		self.currentscope = funcname
+	
+	def newscope(self):
+		scope = "s"+str(self.scope_no)
+		self.table[scope] = {'name':scope,'type':None,'datatype':None,'parentscope':self.currentscope,'identifiers':{}}
+		self.currentscope=scope
+		self.scope_no+=1
+	
+	def endscope(self):
+		self.currentscope = self.table[self.currentscope]['parentscope'] 
+	
+	def checkscope(self,varname):
+		scope = self.currentscope
+		while scope not in ['main']:
+		
+			if varname in self.table[scope]['identifiers']:
+				return scope
+		
+			scope = self.table[scope]['parentscope']
+		
+		if varname in self.table['main']['identifiers']:
+			return 'main'
+		
+		return None
+	
+	def gettype(self,scope,varname):
+		return self.table[scope]['identifiers'][varname]['datatype']
