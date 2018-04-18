819fb4ebaac501c4101f33fd732061f83eb7263b
Divyat <divyatmahajan@gmail.com>
2018-04-19 00:09:17 +0530

    ....

--- Final Project/src/globalvars.py
+++ Final Project/src/globalvars.py
@@ -9,6 +9,9 @@ def isint(value):
   else:
     return value.isdigit()
 
+#For conditionals
+special_count=0
+
 bb = [] #list for basic block
 statements = [] #list of IR objects for each statement
 NextUseTable= []
@@ -18,6 +21,10 @@ labels = {} #stores names of labels if they are line_no as labelline_no
 RegisterStatus={ '%ebx':-1, '%ecx':-1, '%esi':-1, '%edi':-1, '%eax':-1, '%edx':-1 }   
 RegisterData= {'%ebx':None, '%ecx':None, '%esi':None, '%edi':None, '%eax':None, '%edx':None }   
 RegisterAssigned= {  }
+
+FloatRegisterStatus={ '%st0':-1, '%st1':-1, '%st2':-1, '%st3':-1, '%st4':-1, '%st5':-1, '%st6':-1, '%st7' : -1}   
+FloatRegisterData= { '%st0':None, '%st1': None, '%st2': None, '%st3': None, '%st4': None, '%st5': None, '%st6': None, '%st7' : None}
+FloatRegisterAssigned= { }
 
 op2wrd= { '+': 'addl', '-': 'subl', '*': 'imul', '/': 'divl', '%': 'modl', '<<': 'shll', '>>': 'shrl', '&': 'andl', '|': 'orl', '^':'xorl',
 		'ifgoto_gt':'jg', 'ifgoto_geq':'jge' , 'ifgoto_lt':'jl', 'ifgoto_leq':'jle', 'ifgoto_eq':'je', 'ifgoto_neq':'jne' }
